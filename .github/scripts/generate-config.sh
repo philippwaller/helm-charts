b3b988afa0b59e863d54064c54460b7c1c3f980f
Philipp Waller <1090452+philippwaller@users.noreply.github.com>
2022-12-11 12:08:06 +0100

    fix: refactor configuration script to avoid unnecessary version checks

--- .github/scripts/generate-config.sh
+++ .github/scripts/generate-config.sh
@@ -1,35 +1,47 @@
 #!/usr/bin/env bash
 
-ciChanged=$1
-changedCharts=$2
-
+ciChanged="$1"
+changedCharts="$2"
 projectRootPath=$(git rev-parse --show-toplevel)
-if [ "$ciChanged" == false ]; then
-	chartStr=$(echo "$changedCharts" |
-		jq -r .[] |
-		awk -F '/' '{ print $1"/"$2 }' |
-		sort |
-		uniq)
+
+# get changed charts
+changedChartStr=$(echo "$changedCharts" |
+	jq -r .[] |
+	awk -F '/' '{ print $1"/"$2 }' |
+	sort |
+	uniq)
+changedCharts=()
+while IFS='' read -r line; do changedCharts+=("$line"); done < <(echo "$changedChartStr")
+
+if [ "$ciChanged" == true ]; then
+	# get all charts
+	allChartsStr=$(find "$projectRootPath/charts" -type d -mindepth 1 -maxdepth 1 | sed "s~$projectRootPath/~~")
 	charts=()
-	while IFS='' read -r line; do charts+=("$line"); done < <(echo "$chartStr")
+	while IFS='' read -r line; do charts+=("$line"); done < <(echo "$allChartsStr")
 else
-	chartStr=$(find "$projectRootPath/charts" -type d -mindepth 1 -maxdepth 1 | sed "s~$projectRootPath/~~")
-	charts=()
-	while IFS='' read -r line; do charts+=("$line"); done < <(echo "$chartStr")
+	charts=("${changedCharts[@]}")
 fi
 
 index=0
 chartsJsonArray=()
 matrixJsonArray=()
 for chartPath in "${charts[@]}"; do
+
+	if printf '%s\0' "${changedCharts[@]}" | grep -Fxqz -- "$chartPath"; then
+		versionBump=true
+	else
+		versionBump=false
+	fi
+
 	chartName=$(basename "$chartPath")
 	settings=$(yq -o=json '.' "$projectRootPath/$chartPath/ci.yaml")
 	chartJson=$(
 		jq --null-input \
 			--arg name "$chartName" \
 			--arg path "$chartPath" \
+			--arg versionBump "$versionBump" \
 			--argjson settings "$settings" \
-			'{"name": $name, "path": $path, "settings": $settings}'
+			'{"name": $name, "path": $path, "settings": $settings, "versionBump": $versionBump}'
 	)
 	chartsJsonArray+=("$chartJson")
 	versions=()
@@ -42,8 +54,9 @@ for chartPath in "${charts[@]}"; do
 				--arg index "$index" \
 				--arg name "$chartName" \
 				--arg path "$chartPath" \
+				--arg versionBump "$versionBump" \
 				--arg version "$version" \
-				'{"sourceIndex": $index, "name": $name, "path": $path, "version": $version}'
+				'{"sourceIndex": $index, "name": $name, "path": $path, "version": $version, "versionBump": $versionBump}'
 		)
 		matrixJsonArray+=("$matrixJson")
 	done
